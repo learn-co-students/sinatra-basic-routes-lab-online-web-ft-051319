5092737d8027e09b994b6c4138f465d78a336ddd
Kane Koo <kanekoo@gmail.com>
2019-07-01 20:34:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+ get '/name' do
+   status 200
+   "My name is Kane"
+  end
+
+  get '/hometown' do
+    status 200
+    "My hometown is Avenel"
+   end
+
+  get '/favorite-song' do
+    status 200
+    "My favorite song is Live Forever"
+  end
+
 end
